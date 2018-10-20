67197723c4cb34e88cbaefa8ada0ba47a63ed963
Muhammad Elgendi <actnoon@gmail.com>
2018-10-21 01:19:46 +0200

    adding files

---
+++ masking_replace_background.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Developed by : Muhammad Elgendi
+ * Task : Apply foreground to background by using a mask (some image processing with GD library)
+ */
+// Load source and mask and new backgrounds
+$source = imagecreatefrompng( 'sweety.png' );
+$mask = imagecreatefrompng( 'sweety_mask.png' );
+$background = imagecreatefrompng( 'snowy.png' );
+// Apply mask to source with background
+imageAlphaMaskBackground( $source, $mask ,$background );
+// Output
+header( "Content-type: image/png");
+imagepng( $source , "sweety_new.png");
+
+function imageAlphaMaskBackground( &$picture, $mask ,$background ) {
+    // Get sizes and set up new picture
+    $xSize = imagesx( $picture ); // get x of image
+    $ySize = imagesy( $picture ); // get y
+    $newPicture = imagecreatetruecolor( $xSize, $ySize );// Create a new true color image 24-bit  (bit depth) image
+    // sets the flag to attempt to save full alpha channel information (as opposed to single-color transparency) when saving PNG images.
+    imagesavealpha( $newPicture, true ); 
+    // imagefill : Performs a flood fill starting at the given coordinate (top left is 0, 0) with the given color in the image.
+    // imagecolorallocatealpha : Allocate a color for an image with the addition of the transparency parameter alpha.
+    // imagecolorallocatealpha return the color or false
+    // make image transperent
+    imagefill( $newPicture, 0, 0, imagecolorallocatealpha( $newPicture, 0, 0, 0, 127 ) );
+
+    // Perform the equation foreground * alpha + background * 1-alpha or something like this :)
+    /* 
+       imagecolorsforindex — Get the colors for an index 
+       Returns an associative array with red, green, blue and alpha keys that contain the appropriate values for the specified color index.
+    */
+    /*
+      imagecolorat :  Returns the index of the color of the pixel at the specified location in the image specified by image.
+      If the image is a truecolor image, this function returns the RGB value of that pixel as integer. Use bitshifting and masking to access the distinct red, green and blue component values:
+    */
+    // imagesetpixel() draws a pixel at the specified coordinate.
+    for( $x = 0; $x < $xSize; $x++ ) {
+        for( $y = 0; $y < $ySize; $y++ ) {            
+            $alpha = imagecolorsforindex( $mask, imagecolorat( $mask, $x, $y ) );            
+            $backColor = imagecolorsforindex( $background, imagecolorat( $background, $x, $y ) );
+            $alphaZero = $alpha[ 'red' ];
+            imagesetpixel( $newPicture, $x, $y, imagecolorallocatealpha( $newPicture, $backColor[ 'red' ], $backColor[ 'green' ], $backColor[ 'blue' ], $alphaZero ) );
+            $alpha = 127 - floor( $alpha[ 'red' ] / 2 );
+            $color = imagecolorsforindex( $picture, imagecolorat( $picture, $x, $y ) );
+            imagesetpixel( $newPicture, $x, $y, imagecolorallocatealpha( $newPicture, $color[ 'red' ], $color[ 'green' ], $color[ 'blue' ], $alpha ) );
+        }
+    }
+
+    // Copy back to original picture
+    imagedestroy( $picture ); // imagedestroy() frees any memory associated with image image.
+    $picture = $newPicture;
+}
+
+
