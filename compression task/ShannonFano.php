e529ce33f9631099823712acfcbb0c3a606f7e23
Muhammad Elgendi <actnoon@gmail.com>
2018-12-14 17:34:39 +0200

    Adding functionalities to Shannon Fano

--- compression task/ShannonFano.php
+++ compression task/ShannonFano.php
@@ -1,19 +1,53 @@
 <?php
+/**
+ * Developed by : Muhammad Elgendi
+ * Date : 14/12/2018
+ * 
+ * || Notes ||
+ *  This class implemented to comply Fluent interface pattern 
+ *  You can build the tree of Shannon Fano from a message or predefined table
+ */
 
-    $table = ['B'=> 3,'L'=> 2,'E'=> 2,'I'=>1,'A'=>1,'T'=>1,'S'=>1,'N'=>1];
-    $shannon = new ShannonFano($table);
+    // $table = ['B'=> 3,'L'=> 2,'E'=> 2,'I'=>1,'A'=>1,'T'=>1,'S'=>1,'N'=>1];
+    // $shannon = (new ShannonFano())->setTable($table);
+    $shannon =(new ShannonFano())->setMsg('BBBLLEEIATSN');
     $shannon->build_code_book();    
-    print_r($shannon->codeBook);
+    $shannon->encode_msg();
+    print_r($shannon->get_code_book());
+    echo $shannon->get_compressed_code()."\n";
+
 
 class ShannonFano{
 
-    public $table;
-    public $codeBook;
+    private $table;
+    private $codeBook;
+    private $message;
+    private $compressedCode;
 
-    function __construct($table){
+    // Build the table from an assiochative array
+    public function setTable($table){
         arsort($table);
         $this->table = $table;
         $this->codeBook = array_fill_keys(array_keys($this->table), '');
+        return $this;
+    }
+
+    // Build the table from a message
+    public function setMsg($msg){
+        $this->message = $msg;
+        $chars = array_unique(str_split($msg));
+        $frequency = count_chars($msg,1);
+        $table = array_fill_keys(array_values($chars), 0);
+        $keys = array_keys($frequency);
+        $values = array_values($frequency);
+        for($i=0;$i<count($frequency);$i++){
+            $key = chr($keys[$i]);
+            $table[$key] = $values[$i];
+        }
+        arsort($table);
+        $this->table = $table;
+        $this->codeBook = array_fill_keys(array_keys($this->table), '');
+        return $this;
     }
 
     public function build_code_book(){ 
@@ -36,6 +70,22 @@ class ShannonFano{
                 }
             }
         }
+    }
+
+    public function encode_msg(){
+        $this->compressedCode = "";
+        $array = str_split($this->message);
+        foreach($array as $letter){
+                $this->compressedCode .= $this->codeBook[$letter];
+        }
+    }
+
+    public function get_code_book(){
+        return $this->codeBook;
+    }
+
+    public function get_compressed_code(){
+        return $this->compressedCode;
     }
 
     private function get_sepration_point($frequency){
